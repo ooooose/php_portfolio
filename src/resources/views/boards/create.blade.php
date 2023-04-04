988024415c1bc231c08efbde2a52180beac7220b
ooooose <yuuki.oose1990@gmail.com>
2023-04-04 18:14:47 +0900

    [Add]oldヘルパ関数を追加

--- src/resources/views/boards/create.blade.php
+++ src/resources/views/boards/create.blade.php
@@ -19,7 +19,7 @@
                                     <div class="p-2 w-full">
                                     <div class="relative">
                                         <label for="title" class="leading-7 text-sm text-gray-600">タイトル</label>
-                                        <input type="text" id="title" name="title" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        <input type="text" id="title" name="title" value="{{ old('title') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     </div>
                                     </div>
                                     <div class="p-2 w-full">
@@ -42,7 +42,7 @@
                                     <div class="p-2 w-full">
                                     <div class="relative">
                                         <label for="body" class="leading-7 text-sm text-gray-600">内容</label> 
-                                        <textarea id="body" name="body" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                        <textarea id="body" name="body" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ old('body') }}</textarea>
                                     </div>
                                     </div>
                                     <div class="p-2 w-full">

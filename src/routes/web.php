4a7dee1c31be99d452ab3d41ea049fd73605af07
ooooose <yuuki.oose1990@gmail.com>
2023-04-07 16:46:52 +0900

    [Add]サムネイルアップロード時のプレビュー実装（Ajax）

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,8 +22,8 @@ Route::prefix('boards')->middleware(['auth'])
     ->name('boards.')
     ->group(function(){
         Route::get('/', 'index')->name('index');
-        Route::get('/{id}', 'show')->name('show');
         Route::get('/create', 'create')->name('create');
+        Route::get('/{id}', 'show')->name('show');
         Route::post('/', 'store')->name('store');
         Route::get('/{id}/edit', 'edit')->name('edit');
         Route::post('/{id}', 'update')->name('update');

71f3cca4f16d9aad66cb116538a0da99d7a1f675
ooooose <yuuki.oose1990@gmail.com>
2023-04-10 11:03:29 +0900

    [Add]日本語化対応

--- src/resources/views/auth/forgot-password.blade.php
+++ src/resources/views/auth/forgot-password.blade.php
@@ -2,12 +2,12 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-large-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('登録されているメールアドレスを入力してください。メールが届きましたら、パスワード更新の手続きを進めてください。') }}
         </div>
 
         <!-- Session Status -->
@@ -21,14 +21,14 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('メールアドレス')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <x-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('メール送信') }}
                 </x-button>
             </div>
         </form>

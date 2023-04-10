2ecb8dd127277ff95c8e7bd07bf60b465926c0e3
ooooose <yuuki.oose1990@gmail.com>
2023-04-10 11:25:41 +0900

    [Fix]レイアウトの微調整

--- src/resources/views/boards/index.blade.php
+++ src/resources/views/boards/index.blade.php
@@ -27,7 +27,7 @@
                                 <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                                     <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ asset('storage/'.$board->img_path) }}" alt="blog">
                                     <div class="p-6">
-                                        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">No.{{ $board->id }} {{ $board->user->name }}さん</h2>
+                                        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{{ $board->user->name }}さん</h2>
                                         <h1 class="title-font text-lg font-medium text-gray-900">
                                             {{ $board->title }} 
                                             <small>

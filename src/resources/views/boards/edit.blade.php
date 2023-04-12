99e5ad8c01a6dbd417b5d20b7b7f1a9f6212c011
ooooose <yuuki.oose1990@gmail.com>
2023-04-12 10:38:05 +0900

    [Add]本番環境との条件分岐追加

--- src/resources/views/boards/edit.blade.php
+++ src/resources/views/boards/edit.blade.php
@@ -34,7 +34,11 @@
                                         <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">サムネイル</label>
                                         <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                                             <div class="text-center">
-                                            <img src="{{ asset('storage/'. $board->img_path) }}" id='preview'>
+                                            @if (App::environment('local'))
+                                                <img src="{{ asset('storage/'. $board->img_path) }}" id='preview'>
+                                            @else
+                                                <img src="{{ Storage::disk('s3')->url($board->img_path) }}" id='preview'>
+                                            @endif
                                             <div class="mt-4 flex text-sm leading-6 text-gray-600">
                                                 <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                                                     <span>画像をアップロード</span>
